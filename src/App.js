81cc5d7e8b12a810a38b13386a6e5d2b070e200f
unknown <12491862+Vitaljano@users.noreply.github.com>
2022-08-19 00:16:30 +0300

    feat: add basic routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import "./App.css";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Main from './pages/main';
+import TextBook from './pages/textbook';
+
+const APP_PAGES = {
+  main: '/',
+  textBook: '/textbook',
+};
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Main />} path={APP_PAGES.main} />
+        <Route element={<TextBook />} path={APP_PAGES.textBook} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
